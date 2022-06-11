3f4c7df605004d01ba5a2e7bf0c004df4115fec4
pborissow <peter.borissow@yahoo.com>
2022-06-10 20:00:38 -0400

    Updated insert statements in BulkCompare

--- src/main/java/bluewave/test/BulkCompare.java
+++ src/main/java/bluewave/test/BulkCompare.java
@@ -3,40 +3,36 @@ package bluewave.test;
 import bluewave.Config;
 import bluewave.app.Document;
 import bluewave.utils.Python;
-import static bluewave.utils.Python.getScripts;
 import bluewave.utils.StatusLogger;
 import bluewave.web.services.DocumentService;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
-import javaxt.io.Directory;
-import javaxt.io.File;
-import javaxt.json.JSONObject;
-import javaxt.sql.Connection;
-import javaxt.sql.Database;
-import static javaxt.utils.Console.*;
-import javaxt.utils.ThreadPool;
+import static bluewave.utils.Python.*;
+import bluewave.document.analysis.Utils;
+
+import java.util.*;
 import java.sql.SQLException;
 import java.time.Duration;
-import javaxt.sql.Recordset;
-import static bluewave.utils.Python.*;
+import java.util.concurrent.atomic.AtomicLong;
+
+
+import javaxt.io.Directory;
+import javaxt.io.File;
+import javaxt.json.*;
+import javaxt.sql.*;
+import static javaxt.utils.Console.*;
+import javaxt.utils.ThreadPool;
+
+
+
 
 public class BulkCompare {
 
     public static String scriptVersion;
 
     public static void benchmark(HashMap<String, String> args) throws Exception {
-        Config.initDatabase();
 
-        // Check if should update schema
-        updateSchema(Config.getDatabase());
+        Config.initDatabase();
+        Utils.initModels(Config.getDatabase());
+
 
         int threadsNum = -1;
         String dir = null;
@@ -78,7 +74,7 @@ public class BulkCompare {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
         p("deleteCached: " + deleteCached);
         if(deleteCached) {
             //Delete json sidecar files
@@ -104,7 +100,7 @@ public class BulkCompare {
 
             //Create jsoncached files
             final javaxt.io.File script = pyFiles[0];
-            
+
             //Create thread pool used to create jsoncached files
             ThreadPool pool = new ThreadPool(threadsNum) {
                 public void process(Object obj) {
@@ -118,11 +114,11 @@ public class BulkCompare {
                   }catch(Exception e) {
                       e.printStackTrace();
                   }
-                  
+
                 }
             }.start();
             java.time.Instant start = java.time.Instant.now();
-            
+
             // Add docs to pool
             file = null;
             docs = documentDirectory.getChildren(true, "*.pdf");
@@ -132,7 +128,7 @@ public class BulkCompare {
                     pool.add(file);
                 }
             }
-            
+
             pool.done();
 
             try {
@@ -140,7 +136,7 @@ public class BulkCompare {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            
+
             java.time.Instant end = java.time.Instant.now();
 
             Duration duration = Duration.between(start, end);
@@ -172,7 +168,8 @@ public class BulkCompare {
      */
     public static void compare(HashMap<String, String> args) throws Exception {
 
-        Config.initDatabase();
+        //Config.initDatabase();
+        //Utils.initModels(Config.getDatabase());
 
         final int threadsNum;
         final String scriptVer = getScriptVersion();
@@ -224,7 +221,7 @@ public class BulkCompare {
 
                     // Get Documents
                     String[] docs = new String[]{a, b};
-                    
+
                     java.time.Instant pairStartTime = java.time.Instant.now();
                     JSONObject result = DocumentService.getSimilarity(
                             new String[]{a, b},
@@ -242,13 +239,13 @@ public class BulkCompare {
                     //rs.setValue("INFO", result);
                     JSONObject elapsedTimeObj = result.get("elapsed_time_sec").toJSONObject();
                     if(elapsedTimeObj != null) {
-                        rs.setValue("POST_PROC", result.get("post_processing").toDouble());
-                        rs.setValue("TOTAL_SEC", elapsedTimeObj.get("total_sec").toDouble());
-                        rs.setValue("PAGES_PER_SEC", elapsedTimeObj.get("pages_per_second").toDouble());
-                        rs.setValue("READ_PDF", elapsedTimeObj.get("read_pdf").toDouble());
+                        rs.setValue("PY_POST_PROC", result.get("post_processing").toDouble());
+                        rs.setValue("PY_TOTAL_SEC", elapsedTimeObj.get("total_sec").toDouble());
+                        rs.setValue("PY_PAGES_PER_SEC", elapsedTimeObj.get("pages_per_second").toDouble());
+                        rs.setValue("PY_READ_PDF", elapsedTimeObj.get("read_pdf").toDouble());
                     } else {
-                        rs.setValue("ELAPSED_TIME_SEC", result.get("elapsed_time_sec").toDouble());
-                        rs.setValue("PAGES_PER_SEC", result.get("pages_per_second").toDouble());
+                        rs.setValue("PY_ELAPSED_TIME_SEC", result.get("elapsed_time_sec").toDouble());
+                        rs.setValue("PY_PAGES_PER_SEC", result.get("pages_per_second").toDouble());
                     }
                     rs.update();
 
@@ -347,7 +344,7 @@ public class BulkCompare {
         }
 
         java.time.Instant end = java.time.Instant.now();
-        
+
         Duration duration = Duration.between(start, end);
         long days = duration.toDaysPart();
         long hours = duration.toHoursPart();
@@ -405,77 +402,7 @@ public class BulkCompare {
         return docIds;
     }
 
-    private static String rtrim(String s) {
-        int i = s.length() - 1;
-        while (i >= 0 && Character.isWhitespace(s.charAt(i))) {
-            i--;
-        }
-        return s.substring(0, i + 1);
-    }
 
-    private static void updateSchema(Database database) throws Exception {
-
-        Connection conn = null;
-        try {
-            conn = database.getConnection();
-            
-            Boolean exists = false;
-            for (javaxt.sql.Table table : Database.getTables(conn)){
-                String tableName = table.getName().toUpperCase();
-                if (tableName.endsWith("DOCUMENT_COMPARISON_STATS")){
-                    exists = true;
-                    break;
-                }
-            }
-
-
-            if (!exists) {
-                p("Schema additions do not exist, updating ..");
-                // Get updates from file
-                StringBuilder sb = new StringBuilder();
-                InputStream in = BulkCompare.class.getResourceAsStream("/benchmark.sql");
-
-                BufferedReader br = new BufferedReader(new InputStreamReader(in));
-
-                String line;
-                while ((line = br.readLine()) != null) {
-                    sb.append(line + System.lineSeparator());
-                }
-
-
-                //Split updates into individual statements
-                ArrayList<String> statements = new ArrayList<String>();
-                for (String s : sb.toString().split(";")) {
-
-                    StringBuffer str = new StringBuffer();
-                    for (String i : s.split("\r\n")) {
-                        if (!i.trim().startsWith("--") && !i.trim().startsWith("COMMENT ")) {
-                            str.append(i + "\r\n");
-                        }
-                    }
-
-                    String cmd = str.toString().trim();
-                    if (cmd.length() > 0) {
-                        statements.add(rtrim(str.toString()) + ";");
-                    }
-                }
-
-                // Update database schema
-                java.sql.Statement stmt = conn.getConnection().createStatement();
-                for (String cmd : statements) {
-                    stmt.execute(cmd);
-                }
-                p("Schema updated.");
-                stmt.close();
-            } 
-            
-            conn.close();
-        } 
-        catch (Exception e) {
-            if (conn!=null) conn.close();
-            throw e;
-        }
-    }
 
     private static String getScriptVersion() {
         if (scriptVersion == null) {
